6cee86c2650f2ca607897b992f9e27c19c2a7958
towelsh <tea@towel.sh>
2024-11-02 20:47:02 +0000

    prepare for deployment to cloudflare

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,3 @@
-<script>import "../app.css";</script>
+<script>import "../app.css"; import "../app.scss";</script>
 <slot></slot>
 <footer class="text-xs text-gray-500 text-center pt-2">made with ❤ by towel.codes | open source | icons provided by <a href="https://icons8.com">icons8</a></footer>
